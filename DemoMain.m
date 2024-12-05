6c946cbbe478b9c3936a827af6c5d0d4af5c3bf1
yunscage <142385099+yunscage@users.noreply.github.com>
2024-12-05 12:38:09 +0800

    DemoMain.m

--- DemoMain.m
+++ DemoMain.m
@@ -11,14 +11,13 @@ Mylayers=[
     % regressionLayer
     ];
 
-MyOptions = trainingOptions('adam', ...
-    'MaxEpochs', 600, ...
-    'Shuffle', 'every-epoch', ...
+MyOptions = struct('MaxEpochs', 600, ...
     'InitialLearnRate', 1e-3, ...
-    'Verbose', false, ...
-    'Plots', 'training-progress', ...
-    'L2Regularization', 0, ... % 加入L2正则化
-    'ExecutionEnvironment', 'gpu'); % 使用GPU加速
+    'ExecuEnvironment', 'gpu', ...% 使用GPU加速
+     'L2Regularization', 0, ... % 加入L2正则化
+     'updateRate',2.5e-5, ... 
+     'Consheld', 0.05, ... % 贡献度激活阈值
+     'Plots', 'training-progress'); % training-progress none
 
 ThisDataX=[];
 ThisDataY=[];
@@ -30,36 +29,31 @@ for cnt=1:length(DataY)
     ThisDataX=DataX{cnt};
     ThisDataY=DataY{cnt}/DataY{cnt}(1);
 
-     % plot(DataX{cnt}(7,:),DataY{cnt}/DataY{cnt}(1));hold on;
-     % title(num2str(cnt));
 
-% ThisDataX(7,:)=ThisDataX(7,:)/2;
-% ThisDataX(8,:)=ThisDataX(8,:)/3;
-% ThisDataX(9,:)=ThisDataX(9,:)/3;
-%% 使用自定义函数更新神经网络
-Prenet=dlnetwork(Mylayers);
-XTrain = dlarray(ThisDataX, 'CB'); 
-YTrain = dlarray(ThisDataY, 'CB'); 
-if cnt==1
-    Thisnet=trainCustomNetwork(Prenet,XTrain,YTrain,MyOptions);
-else
-    MyOptions.InitialLearnRate=5e-4;
-    MyOptions.L2Regularization=5e-4;
-    Thisnet=trainCustomNetwork(Thisnet,XTrain,YTrain,MyOptions);
-end
-ypred=forward(Thisnet,XTrain);
+    %% 使用自定义函数更新神经网络
+    Prenet=dlnetwork(Mylayers);
+    XTrain = dlarray(ThisDataX, 'CB');
+    YTrain = dlarray(ThisDataY, 'CB');
+    if cnt==1
+        Thisnet=trainCustomNetwork_v2(XTrain,YTrain,Mylayers,MyOptions);
+    else
+        MyOptions.InitialLearnRate=5e-4;
+        MyOptions.L2Regularization=5e-4;
+        Thisnet=trainCustomNetwork_v2(XTrain,YTrain,Thisnet.Layers,MyOptions);
+    end
+    ypred=forward(Thisnet,XTrain);
 
-ypredpre=forward(Thisnet,PreXTrain);
-figure;
-plot(ThisDataX(7,:),ThisDataY,'k');hold on;
-plot(ThisDataX(7,:),ypred,'r');
+    ypredpre=forward(Thisnet,PreXTrain);
+    figure;
+    plot(ThisDataX(7,:),ThisDataY,'k');hold on;
+    plot(ThisDataX(7,:),ypred,'r');
 
-rmsetotal=extractdata(sqrt(mean((ThisDataY-ypred).^2)));
-rmse1=extractdata(sqrt(mean((PreYTrain-ypredpre).^2)));
-disp('******Result******');
-disp('RMSEtotal=');
-disp(rmsetotal);
-disp('RMSE1=');
-disp(rmse1);
-pause(0.5);
+    rmsetotal=extractdata(sqrt(mean((ThisDataY-ypred).^2)));
+    rmse1=extractdata(sqrt(mean((PreYTrain-ypredpre).^2)));
+    disp('******Result******');
+    disp('RMSEtotal=');
+    disp(rmsetotal);
+    disp('RMSE1=');
+    disp(rmse1);
+    pause(0.5);
 end
