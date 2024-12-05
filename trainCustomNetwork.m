83cdc5e9cbe4c5a78ef39d4584eb77aa2557f6b9
yunscage <142385099+yunscage@users.noreply.github.com>
2024-12-05 12:38:42 +0800

    trainCustomNetwork_v2.m

--- trainCustomNetwork.m
+++ trainCustomNetwork.m
@@ -1,170 +1,219 @@
-function [net, info] = trainCustomNetwork(dlnet, XTrain, YTrain, options)
-% V2.0 加入了成熟度的更新，L2正则化
-    % 将数据移到 GPU 上
-    dlX = gpuArray(XTrain);
-    dlY = gpuArray(YTrain);
-    
-    % 训练网络
-    numEpochs = options.MaxEpochs;
-    InitialLearnRate=options.InitialLearnRate;
-    L2Lamda=options.L2Regularization;
-    if L2Lamda>0
-        preWeights=dlnet.Learnables.Value;
-    else
-        preWeights=[];
-    end
-    % 初始化 Adam 优化器的动量变量
-    % Adam 优化器的参数
-    decayRate=0.001;
-    
-    updateRate=1e-6;
-    % beta1 = 0.9;
-    % beta2 = 0.999;
-    % epsilon = 1e-8;
-    %% 贡献度参数
+function [net, info] = trainCustomNetwork_v2( XTrain, YTrain, dlLayers,options)
+dlnet=dlnetwork(dlLayers);
+% 将数据移到 GPU 上
+dlX = gpuArray(XTrain);
+dlY = gpuArray(YTrain);
+
+% 训练网络
+numEpochs = options.MaxEpochs;
+InitialLearnRate=options.InitialLearnRate;
+L2Params.L2Lamda=options.L2Regularization;
+L2Params.LearnWeights=dlnet.Learnables.Value;
+if L2Params.L2Lamda<=0
+    L2Params.LearnWeights=[];
+end
+updateRate=options.updateRate;
+ConSheld=options.Consheld;% 贡献度更新百分比
+% 初始化 Adam 优化器的动量变量
+% Adam 优化器的参数
+decayRate = 0.001;  % 学习率衰减
+beta1 = 0.9;
+beta2 = 0.999;
+epsilon = 1e-8;
+
+ %计算权重层 索引
+    LayerCntIdx = find(strncmp(dlnet.Learnables.Parameter, 'Weights',7) |...
+                                strncmp(dlnet.Learnables.Parameter, 'InputWeights',12));
+    LayerCnt=length(LayerCntIdx)-1;
     % 计算输出层 名字
-    ThisConnections=dlnet.Connections(1:end-2,:);
-    LayerIndex=find(strncmp(ThisConnections.Destination, 'fc_',3));%查找fc layer
-    Layername=ThisConnections.Destination(LayerIndex);
-    %计算权重层 索引
-    weightIdx = find(strncmp(dlnet.Learnables.Layer, 'fc_',3) &...
-                                strncmp(dlnet.Learnables.Parameter, 'Weights',7));
-    weightIdx= weightIdx(1:end-1);
-    weightIdx=dlarray(weightIdx,'CB');
+    Layername= unique(dlnet.Learnables.Layer, 'stable'); % 顺序提取不重复的层名
+    Layername=Layername(1:end-1);
+    weightIdx= LayerCntIdx(2:end);
     weightIdx=gpuArray(weightIdx);
-    NodeMaturity=cell(1,length(weightIdx)); % 记录成熟度，迭代的次数
-    NodeConSum = cell(1, length(weightIdx));
+    
     NodeNum=0; % 神经元总个数
-
-    for cnt=1:length(weightIdx)
-        Temp=dlnet.Learnables.Value{weightIdx(cnt)};
-        TempSize=size(Temp,1);
-        NodeNum=NodeNum+TempSize;
-        NodeConSum{cnt}=gpuArray(dlarray(0.5*ones(TempSize,1)));
-        NodeMaturity{cnt}=gpuArray(dlarray(zeros(TempSize,1)));
+    % 成熟度记录
+    NodeComSum=0;% 更新神经元个数
+    NodeConSum = ModelNodeContribution(dlnet, dlX(:,1),Layername,weightIdx);
+    NodeMaturity=cell(1,LayerCnt); % 记录成熟度，迭代的次数
+    for cnt=1:LayerCnt
+        NodeMaturity{cnt} = zeros(size(NodeConSum{cnt}));
+        NodeNum=NodeNum + length(NodeConSum{cnt});
     end
-    SampleNum=dlarray(length(dlY)*NodeNum); % 每次更新的神经元总次数
+    SampleNum=dlarray(NodeNum); % 每次更新的神经元总次数
     NodeTotalUpdateNum=0;
+% 记录损失值
+lossArray = zeros(1, numEpochs);
+
+% 创建训练进度监视器
+if strcmp(options.Plots, 'training-progress')
+    show_flag = 1;
+    monitor = trainingProgressMonitor;
+    monitor.Metrics = ["TotalLoss","Manurity","NodeUpdate"];
+    monitor.XLabel = "Epoch";
+else
+    show_flag = 0;
+end
+averageGrad=[];
+averageSqGrad=[];
+% 开始训练循环
+for epoch = 1:numEpochs
+    % 动态调整学习率
+    learningRate = InitialLearnRate * (1 / (1 + decayRate * epoch));
+    % 前向传播和损失计算，使用 dlfeval
+    [gradients, loss] = dlfeval(@modelGradients, dlnet, dlX, dlY,L2Params);
+    % 遍历 gradients 表中的每个元素，并添加噪声
+    for i = 1:size(gradients, 1)
+        % 提取当前的梯度值
+        gradValue = gradients.Value{i};
+        % 生成与当前梯度维度匹配的随机噪声，并确保其类型与梯度一致  GPU or CPU
+        noise = 0.2*learningRate * randn(size(gradValue), 'like', gradValue);  % 'like' 保证类型一致
+        % 将噪声添加到梯度值中
+        gradients.Value{i} = gradValue + noise;
+    end
+
+    % Adam Update / Adam 更新
+    [dlnet, averageGrad, averageSqGrad] = ...
+        adamupdate( dlnet, gradients, averageGrad, averageSqGrad,epoch,learningRate,...
+        beta1,beta2,epsilon);
+
+
+    %% 贡献度相关计算
+    % 计算贡献度
+    NodeCon = ModelNodeContribution(dlnet, dlX,Layername,weightIdx);
+    % 更新贡献度和成熟度
+    for cnt = 1:LayerCnt
+        NodeConSum{cnt} = 0.7*NodeConSum{cnt} + 0.3*NodeCon{cnt};
+        NodeMaturity{cnt} = NodeMaturity{cnt}+1;
+    end
+    % 检查更新节点
+    NodeTotalUpdateNum=NodeTotalUpdateNum+SampleNum;
+    if NodeTotalUpdateNum*updateRate>=1% 触发更新机制
+        NodeTotalUpdateNum=0;
+        nodeset=[];
+        for cnt = 1:LayerCnt
+            % 检查节点贡献度
+            XLogi=(NodeConSum{cnt} < ConSheld &NodeMaturity{cnt} >50);
+            XLogi=find(extractdata(XLogi));
+            if ~isempty(XLogi)
+                nodeset=[nodeset;[cnt*ones(length(XLogi),1) XLogi]];
+            end
+        end
+        if ~isempty(nodeset)
+            UpdateNodeindex=randi(size(nodeset,1));
+            % 获取要更新的层数和节点索引
+            selLayer = nodeset(UpdateNodeindex, 1);% 层数
+            selNeuron = nodeset(UpdateNodeindex, 2);% 节点索引
+            dlnet = ResetNeuronParameters(dlnet, Layername(selLayer), selNeuron);
+            NodeComSum=NodeComSum+1;
+            NodeMaturity{selLayer}(selNeuron)=0;
+        end
+    end
+    TRate=0;
+    for cnt = 1:LayerCnt
+        % 贡献度低于0.04 并且成熟度大于100
+        TRate=TRate + sum((NodeConSum{cnt} > 1.1*ConSheld));
+    end
+
     % 记录损失值
-    lossArray = zeros(1, numEpochs);
-    NeuRate = zeros(1, numEpochs);
-    % 创建训练进度监视器
-    if strcmp(options.Plots, 'training-progress')
-        show_flag = 1;
-        monitor = trainingProgressMonitor;
-        monitor.Metrics = ["TotalLoss", "Maturate"];
-        monitor.XLabel = "Epoch";
-    else
-        show_flag = 0;
+    lossArray(epoch) = extractdata(loss);
+
+    if show_flag
+        recordMetrics(monitor, epoch, "TotalLoss", loss,...
+            "Manurity",TRate/NodeNum,...
+            "NodeUpdate",NodeComSum);
+        monitor.Progress = 100 * epoch / numEpochs;
     end
-    averageGrad=[];
-    averageSqGrad=[];
-    % 开始训练循环
-    for epoch = 1:numEpochs
-        % 动态调整学习率
-        learningRate = InitialLearnRate * (1 / (1 + decayRate * epoch));
-        % 前向传播和损失计算，使用 dlfeval
-        [gradients, loss, loss_mse] = dlfeval(@modelGradients, dlnet, dlX, dlY,preWeights,L2Lamda);
-        % 计算贡献度
-        [NodeCon] = dlfeval(@ModelNodeContribution, dlnet, dlX,Layername,weightIdx);
-        
-        % 更新贡献度和成熟度
-        for cnt = 1:length(NodeCon)
-            NodeConSum{cnt} = 0.8*NodeConSum{cnt} + 0.2*NodeCon{cnt};
-            NodeMaturity{cnt} = NodeMaturity{cnt}+1;
-        end
+end
 
-        % 遍历 gradients 表中的每个元素，并添加噪声
-        for cnt = 1:size(gradients, 1)
-            % 提取当前的梯度值
-            gradValue = gradients.Value{cnt};
-            % 生成与当前梯度维度匹配的随机噪声，并确保其类型与梯度一致  GPU or CPU
-            noise = 0.5*learningRate * randn(size(gradValue), 'like', gradValue);  % 'like' 保证类型一致
-            % 将噪声添加到梯度值中
-            gradients.Value{cnt} = gradValue + noise;
-        end
-
-        % Adam Update / Adam 更新
-        [dlnet, averageGrad, averageSqGrad] = ...
-            adamupdate( dlnet, gradients, averageGrad, averageSqGrad,epoch,learningRate);
-        % 记录损失值
-        lossArray(epoch) = extractdata(loss);
-        % 检查更新节点
-        NodeTotalUpdateNum=NodeTotalUpdateNum+SampleNum;
-        if NodeTotalUpdateNum*updateRate>=1% 触发更新机制
-           NodeTotalUpdateNum=0;
-           nodeset=[];
-           for cnt = 1:length(weightIdx)
-               % 贡献度低于0.04 并且成熟度大于100
-               XLogi=(NodeConSum{cnt} < 0.45 &NodeMaturity{cnt} >150);
-               XLogi=find(extractdata(XLogi));
-               if ~isempty(XLogi)
-                   nodeset=[nodeset;[cnt*ones(length(XLogi),1) XLogi]];
-               end
-           end
-           if ~isempty(nodeset)
-               UpdateNodeindex=randi(size(nodeset,1));
-               % 获取要更新的层数和节点索引
-               selLayer = nodeset(UpdateNodeindex, 1);
-               selNeuron = nodeset(UpdateNodeindex, 2);
-               Layerindx=weightIdx(selLayer);
-               % weight update
-               ThisNeuronPara=dlnet.Learnables.Value{Layerindx}(selNeuron,:);
-               ThisNeuronPara=randn(size(ThisNeuronPara), 'like', ThisNeuronPara)/3;
-               dlnet.Learnables.Value{Layerindx}(selNeuron,:)=ThisNeuronPara;
-               % bias update
-               ThisNeuronPara=dlnet.Learnables.Value{Layerindx+1}(selNeuron,:);
-               ThisNeuronPara=randn([1 1], 'like', ThisNeuronPara)/3;
-               dlnet.Learnables.Value{Layerindx+1}(selNeuron,:)=ThisNeuronPara;
-               NodeMaturity{selLayer}(selNeuron)=0;
-           end
-        end
-        TRate=0;
-       for cnt = 1:length(weightIdx)
-               % 贡献度低于0.04 并且成熟度大于100
-               TRate=TRate + sum((NodeConSum{cnt} > 0.5));
-       end
-       NeuRate(epoch)=TRate/NodeNum;
-        if show_flag
-            recordMetrics(monitor, epoch, "TotalLoss", loss, "Maturate", TRate/NodeNum);
-            monitor.Progress = 100 * epoch / numEpochs;
-        end
-    end
-
-    % 返回训练后的网络和损失信息
-    net = dlnet;
-    info = struct('Loss', lossArray);
-    info.ManuRate=NeuRate;
+% 返回训练后的网络和损失信息
+net = dlnet;
+info = struct('Loss', lossArray);
 end
 
 
-function [gradients, loss, loss_mse] = modelGradients(dlnet, dlX, dlY,preWeights,L2Lamda)
-    % 前向传播
-     dlYpred = forward(dlnet, dlX);
-    % 计算损失
-    Error=dlYpred - dlY;
-    loss_mse = mean((Error).^2);
-    L2Loss=0;
-    if L2Lamda<=0
+function [gradients, loss] = modelGradients(dlnet, dlX, dlY,L2Params)
+% 前向传播
+dlYpred = forward(dlnet, dlX);
+% 计算损失
+Error=dlYpred - dlY;
+loss_mse = mean((Error).^2);
+L2Loss=0;
+L2Lamda=L2Params.L2Lamda;
+if L2Lamda<=0
+    preWeights=L2Params.LearnWeights;
     for cnt=1:length(preWeights)
         L2Loss=L2Loss+sum(sum((dlnet.Learnables.Value{cnt}-preWeights{cnt}).^2));
     end
-    end
-    loss=mean((Error).^2)+ L2Lamda*L2Loss;%tanh(loss_mse);
-    % 计算梯度
-    gradients = dlgradient(loss, dlnet.Learnables);
+end
+loss=loss_mse+ L2Lamda*L2Loss;
+% 计算梯度
+gradients = dlgradient(loss, dlnet.Learnables);
 end
 
 function [NodeCon] = ModelNodeContribution(dlnet, dlX,Layername,Windx)
 % 计算节点权重 NodeCon
 LayerCnt=length(Layername);
-HiddenOut=cell(1,LayerCnt);
-LocalWeight=cell(1,LayerCnt);
 NodeCon=cell(1,LayerCnt);
 for cnt=1:LayerCnt
-    HiddenOut{cnt}= forward(dlnet, dlX,'Outputs',Layername{cnt});
-    LocalWeight{cnt}=dlnet.Learnables.Value{Windx(cnt)};
-    colSums = sum(abs(LocalWeight{cnt}), 2);
-    NodeCon{cnt}=mean(colSums.*abs(HiddenOut{cnt}),2);
+    HiddenOut= forward(dlnet, dlX,'Outputs',Layername{cnt});% Output hidden stste
+    HidOut=mean(abs(stripdims(HiddenOut)),2);
+    LocalWeight=dlnet.Learnables.Value{Windx(cnt)};
+    colSums = sum(abs(LocalWeight), 1)';
+    NodeCon{cnt}=colSums.*HidOut;
 end
+end
+
+function dlnet = ResetNeuronParameters(dlnet, layerName, neuronIndex)
+% 更新 dlnet 中指定层的特定神经元的参数
+%
+% 输入参数:
+% dlnet          - 深度学习网络对象 (dlnetwork)
+% layerName      - 要更新的层的名称 (字符串)
+% neuronIndex    - 要更新的神经元索引 (整数)
+%
+% 输出参数:
+% dlnet          - 更新后的深度学习网络对象
+
+    % 查找目标层参数
+    LayerParams = dlnet.Learnables;
+    rows = LayerParams.Layer == layerName;
+    Para_Value=LayerParams.Value;
+    % 更新 FullyConnected 层的参数
+    Sel_Row = LayerParams.Parameter == "Weights";
+    if any(rows & Sel_Row)
+        % 获取并更新 Weights
+        Sel_Para = Para_Value{rows & Sel_Row};
+        Sel_Para(neuronIndex, :) = randn(size(Sel_Para(neuronIndex, :)), 'like', Sel_Para)/3;
+        Para_Value{rows & Sel_Row}= Sel_Para;
+
+        % 获取并更新 Bias
+        Sel_Row = LayerParams.Parameter == "Bias";
+        Sel_Para = Para_Value{rows & Sel_Row};
+        Sel_Para(neuronIndex, :) = randn(size(Sel_Para(neuronIndex, :)), 'like', Sel_Para)/3;% 更新偏置
+        Para_Value{rows & Sel_Row} = Sel_Para;
+    end
+
+    % 更新 LSTM 层的参数
+    Sel_Row = LayerParams.Parameter == "InputWeights";
+    if any(rows & Sel_Row)
+        % 获取并更新 InputWeights
+        lstmneuronIndex=4*(neuronIndex-1)+1:4*neuronIndex;
+        Sel_Para = Para_Value{rows & Sel_Row};
+        Sel_Para(lstmneuronIndex, :) = randn(size(Sel_Para(lstmneuronIndex, :)), 'like', Sel_Para)/3; 
+        Para_Value{rows & Sel_Row} = Sel_Para;
+
+        % 获取并更新 RecurrentWeights
+        Sel_Row = LayerParams.Parameter == "RecurrentWeights";
+        Sel_Para = Para_Value{rows & Sel_Row};
+        Sel_Para(lstmneuronIndex, :) = randn(size(Sel_Para(lstmneuronIndex, :)), 'like', Sel_Para)/3; 
+        Para_Value{rows & Sel_Row} = Sel_Para;
+
+        % 获取并更新 Bias
+        Sel_Row = LayerParams.Parameter == "Bias";
+        Sel_Para = Para_Value{rows & Sel_Row};
+        Sel_Para(lstmneuronIndex, :) = randn(size(Sel_Para(lstmneuronIndex, :)), 'like', Sel_Para)/3; % 更新偏置
+        Para_Value{rows & Sel_Row} = Sel_Para;
+    end
+    dlnet.Learnables.Value=Para_Value;
 end
